5a73766baece30e5605e984e50496ebf1b99a9d3
Jessica  Fong Ng <jessicafongng@Jessicas-MacBook-Air.local>
2021-04-04 14:51:35 -0400

    bjkst done

--- src/main/scala/project_2/main.scala
+++ src/main/scala/project_2/main.scala
@@ -77,8 +77,7 @@ object main{
 
   class BJKSTSketch(bucket_in: Set[(String, Int)] ,  z_in: Int, bucket_size_in: Int) extends Serializable {
 /* A constructor that requires initialize the bucket and the z value. The bucket size is the bucket size of the sketch. */
-    val c = 32 // constant
-    val epsilon = c/bucket_size_in
+
     var bucket: Set[(String, Int)] = bucket_in
     var z: Int = z_in
 
@@ -90,21 +89,42 @@ object main{
     }
 
     def +(that: BJKSTSketch): BJKSTSketch = {    /* Merging two sketches */
-      var merge_B = this.bucket.union(that.bucket)
-      var merge_z = math.min(this.BJKST_bucket_size,that.BJKST_bucket_size)
-      while(merge_B.size>= 2/epsilon){
-        merge_z += 1
-        merge_B = merge_B.filterNot(s=> s._2<merge_z)
+      bucket = this.bucket | that.bucket
+      z = math.max(this.z, that.z)
+      while(bucket.size >= this.BJKST_bucket_size){
+        z += 1
+        bucket = bucket.filterNot(s=> s._2 < z)
       }
-      return new BJKSTSketch(merge_B,merge_z,this.BJKST_bucket_size)
+      return this
     }
+
 
     def add_string(s: String, z_of_s: Int): BJKSTSketch = {   /* add a string to the sketch */
-      val new_ele = new BJKSTSketch(s,z_of_s,this.BJKST_bucket_size)
-      return this.+(new_ele)
-    }
+      if (z_of_s >= z) {
+         var item = Set((s,z_of_s))
+	bucket = bucket | item
+	
+      while(bucket.size >= this.BJKST_bucket_size){
+		z += 1
+		bucket = bucket.filterNot(s=> s._2 < z)
+	}
+      }
+      return this
   }
+}
 
+
+  def BJKST(x: RDD[String], width: Int, trials: Int) : Double = {
+       val h = Seq.fill(trials)(new hash_function(2000000000))
+
+    def param0 = (a1: Seq[BJKSTSketch], s: String) => Seq.range(0, trials).map(i => a1(i).add_string(s, h(i).zeroes(h(i).hash(s))  )  ) //parallel computing part
+    def param1 = (a1: Seq[BJKSTSketch], a2: Seq[BJKSTSketch]) => Seq.range(0,trials).map(i => (a1(i)) + (a2(i))) //merge bucket
+    val x3 = x.aggregate(Seq.fill(trials)(new BJKSTSketch("dim", 0, width)))( param0, param1)
+     val ans = x3.map(s => scala.math.pow(2, s.z.toDouble)*s.bucket.size.toDouble).sortWith(_ < _)( trials/2) /* Take the median of the trials */
+
+    return ans
+
+  }
 
   def tidemark(x: RDD[String], trials: Int): Double = {
     val h = Seq.fill(trials)(new hash_function(2000000000))
@@ -116,12 +136,6 @@ object main{
     val ans = x3.map(z => scala.math.pow(2,0.5 + z)).sortWith(_ < _)( trials/2) /* Take the median of the trials */
 
     return ans
-  }
-
-
-  def BJKST(x: RDD[String], width: Int, trials: Int) : Double = {
-    val h = Seq.fill(trials)(new hash_function(2000000000))
-    return 0
   }
 
 
@@ -149,7 +163,7 @@ def t_o_w(x: RDD[String]): Long = {
 
 
   def exact_F2(x: RDD[String]) : Long = {
-    return x.map(x => (x, 1.asInstanceOf[Long])).reduceByKey(_+_).map(a=>a._2*a._2).sum.round
+    return x.map(x => (x, 1.toLong)).reduceByKey(_+_).map(a=>a._2*a._2).sum.round
   }
 
 
